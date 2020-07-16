966c55999eabf14e20070b6b8bb9a719063b471c
Vaidehi Tathe <vaidehitathe30@gmail.com>
2020-07-17 02:32:08 +0530

    [Vaidehi]REFACTOR:variables changed

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,52 +1,55 @@
 #!/bin/bash +x
 echo "Welcome to Tic Tac Toe Problem"
-LETTERO=O
-LETTERX=X
-
 declare -a board
-
-
-function displayBoard()
-{
-	echo -e "| ${board[1]} | ${board[2]} | ${board[3]} |\n_____________\n| ${board[4]} | ${board[5]} | ${board[6]} |\n_____________\n| ${board[7]} | ${board[8]} | ${board[9]} |"
-}
 
 function resetBoard()
 {
 	board=(. . . . . . . . . .)
 }
 
+function playerChooseLetter()
+{
+
+        read -p "Choose a letter X or O:" letter
+        if [[ $letter == X ]]
+        then
+                playerLetter=X
+                computerLetter=O
+        else
+                playerLetter=O
+                computerLetter=X
+        fi
+        echo "Player will play first and he chooses letter $playerLetter And Computer letter is $computerLetter"
+
+
+}
+
+function computerChooseLetter()
+{
+        letterCheck=$((RANDOM%2))
+        if [[ $letterCheck -eq $one ]]
+        then
+                computerLetter=O
+                playerLetter=X
+        else
+                playerLetter=O
+                computerLetter=X
+        fi
+        echo "Computer will play first and he chooses letter $computerLetter And Player letter is $playerLetter"
+}
 
 function tossToPlay()
 {
-	toss=$((RANDOM%2))
-	letterCheck=$((RANDOM%2))
-	if [[ $((RANDOM%2)) -eq 0 ]]
-	then
-		read -p "Choose a letter X or O:" l
-		if [[ $l == $LETTERX ]]
-      		then
-         		playerLetter=$l
-         		computerLetter=$LETTERO
-      		else
-         		playerLetter=$l
-         		computerLetter=$LETTERX
-      		fi
-		echo "Player will play first and he chooses letter $playerLetter And Computer letter is $computerLetter"
-	else
-                if [[ $letterCheck -eq $one ]]
-                then
-                        computerLetter=$LETTERO
-                        playerLetter=$LETTERX
-                else
-                        playerLetter=$LETTERO
-                        computerLetter=$LETTERX
-                fi
-		 echo "Computer will play first and he chooses letter $computerLetter And Player letter is $playerLetter"
-
-	fi
+        toss=$((RANDOM%2))
+        case $toss in
+                0)
+                        playerChooseLetter
+                        ;;
+                1)
+                        computerChooseLetter
+                        ;;
+        esac
 }
 
 resetBoard
-displayBoard
 tossToPlay
