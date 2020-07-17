0ea4a560cab834dbb42688ff5d0cb9b33efbb368
Vaidehi Tathe <vaidehitathe30@gmail.com>
2020-07-17 14:16:58 +0530

    [Vaidehi]REFACTOR:indentatiion corrected

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,55 +1,47 @@
 #!/bin/bash +x
 echo "Welcome to Tic Tac Toe Problem"
+NUM_ROWS=3
+NUM=COLS=3
+BOARD_SIZE=$((NUM_ROWS*NUM_COLS))
+
+position=0
 declare -a board
 
-function resetBoard()
+function resetPlayBoard()
 {
-	board=(. . . . . . . . . .)
+	for((position=1;position<=BOARD_SIZE;position++))
+	do
+		board[$position]=0;
+	done
 }
 
-function playerChooseLetter()
+
+function toss()
 {
+	random=$((RANDOM%2))
+		if [ $random -eq 0 ]
+		then
+			read -p "Player will play first,choose your letter X or O" input
+				if [[ $input == X ]]
+				then
+					userLetter=X;
+					compLetter=O;
+				else
+					userLetter=O;
+					compLetter=X;
+				fi
+		else
+			echo "Computer will play first"
+			choose=$((RANDOM%2))
+				if [ $choose -eq 0 ]
+				then
+					userLetter=O;
+					compLetter=X;
+				else
+					userLetter=X;
+					compLetter=O;
+				fi
 
-        read -p "Choose a letter X or O:" letter
-        if [[ $letter == X ]]
-        then
-                playerLetter=X
-                computerLetter=O
-        else
-                playerLetter=O
-                computerLetter=X
-        fi
-        echo "Player will play first and he chooses letter $playerLetter And Computer letter is $computerLetter"
-
-
+		fi
 }
-
-function computerChooseLetter()
-{
-        letterCheck=$((RANDOM%2))
-        if [[ $letterCheck -eq $one ]]
-        then
-                computerLetter=O
-                playerLetter=X
-        else
-                playerLetter=O
-                computerLetter=X
-        fi
-        echo "Computer will play first and he chooses letter $computerLetter And Player letter is $playerLetter"
-}
-
-function tossToPlay()
-{
-        toss=$((RANDOM%2))
-        case $toss in
-                0)
-                        playerChooseLetter
-                        ;;
-                1)
-                        computerChooseLetter
-                        ;;
-        esac
-}
-
-resetBoard
-tossToPlay
+toss
